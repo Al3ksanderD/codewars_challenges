9f81a02bbbc5b37742b1a3d4469e5864ae6b19a6
Al3ksanderD <olo1045@wp.pl>
2022-08-23 20:34:46 +0000

    all the stuff i did today

---
+++ Rot13.py
@@ -0,0 +1,23 @@
+def rot13(message):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    encoded = ""
+    for x in range(len(message)):
+        if message[x].lower() in alphabet:
+            index = alphabet.index(message[x].lower()) + 13
+            if index > (len(alphabet) - 1):
+                index %= len(alphabet)
+                if message[x].isupper():
+                    encoded += alphabet[index].upper()
+                else:
+                    encoded += alphabet[index]
+            else:
+                if message[x].isupper():
+                    encoded += alphabet[index].upper()
+                encoded += alphabet[index]
+        else:
+            encoded += message[x]
+    return encoded
+
+print(rot13("Nibvq fhpprff ng nyy pbfgf!"))
+
